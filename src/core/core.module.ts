2473f521b2c333f8059a1b5acb6da5b70b98dcc1
brunoocn <bruno.caneo@contemplato.com>
2024-07-26 02:35:58 -0300

    refactor: refatorando estrutura dos providers

--- src/core/core.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { ProvidersModule } from './providers/providers.module';
-
-@Global()
-@Module({
-  imports: [ProvidersModule],
-  exports: [ProvidersModule],
-})
-export class CoreModule {}
